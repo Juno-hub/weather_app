693ef0df398ff2ff3d3f56259a6b3a48a2f307af
Juno <bae3054@naver.com>
2020-10-25 22:29:42 +0900

    git push test

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.yellowView} />
-      <View style={styles.blueView} />
+      <View style={styles.blueView}> 
+        <Text>왜 prettier는 작동하지 않는가...?</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
