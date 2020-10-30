865a0b629e09971a2e1448a66267c8da36d888bb
Juno <bae3054@naver.com>
2020-10-30 14:22:13 +0900

    Loading Screen

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView} />
-      <View style={styles.blueView}> 
-        <Text>왜 prettier는 작동하지 않는가...?</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: 'yellow'
-  },
-  blueView: {
-    flex: 1,
-    backgroundColor: 'blue'
-  }
-});
